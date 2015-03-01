1e893112910f123bce52e07a04ef26a96b43d84d
PinalBhattOutlook <Pinal.Bhatt@outlook.com>
2015-03-01 18:54:08 -0500

    commit

--- assets/js/c9.configs.js
+++ assets/js/c9.configs.js
@@ -1,15 +1,25 @@
-//window.c9Configs = {
-//    appUrl: 'http://localhost:12551/',
-//    idSysUrl: 'http://idsys.azurewebsites.net/',
-//    dSvcUrl: 'http://dsvc.pbdesk.com/api/',
-//    clientId: 'C9LocalHost',
-//    allowedRole: 'C9Admin'
-//}
+switch (location.hostname.toLowerCase()) {
+    case 'localhost': {
+        window.c9Configs = {
+            appUrl: 'http://localhost:12551/',
+            idSysUrl: 'http://idsys.azurewebsites.net/',
+            dSvcUrl: 'http://dsvc.pbdesk.com/api/',
+            clientId: 'C9LocalHost',
+            allowedRole: 'C9Admin'
+        }
+        break;
 
-window.c9Configs = {
-    appUrl: 'http://channel9.pbdesk.com/',
-    idSysUrl: 'http://idsys.azurewebsites.net/',
-    dSvcUrl: 'http://dsvc.pbdesk.com/api/',
-    clientId: 'C9GitHubHost',
-    allowedRole: 'C9Admin'
+    }
+    case 'channel9.pbdesk.com': {
+        window.c9Configs = {
+            appUrl: 'http://channel9.pbdesk.com/',
+            idSysUrl: 'http://idsys.azurewebsites.net/',
+            dSvcUrl: 'http://dsvc.pbdesk.com/api/',
+            clientId: 'C9GitHubHost',
+            allowedRole: 'C9Admin'
+        }
+        break;
+
+    }
 }
+
